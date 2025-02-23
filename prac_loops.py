7e1309dd3dfb4e825120df7df8c4dcc08b130bbe
RESEGO MOTLHASI <remotlhjhb024@student.wethinkcode.co.za>
2025-02-23 11:42:06 +0200

    Added code for the reverse pyramid function

--- prac_loops.py
+++ prac_loops.py
@@ -4,4 +4,13 @@ def number_pattern():
         for y in range(1, x+1):
             print(y, end=' ')
         print()
-number_pattern()
+#number_pattern()
+
+def reverse_pyramid():
+    n = int(input("Enter the nth value: "))
+    
+    for x in range(n + 1, 0, -1):
+        for y in reversed(range(x + 1, 0, -1)):
+            print("*", end=" ")
+        print()
+reverse_pyramid()
